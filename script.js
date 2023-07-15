21544cbab223d8325a391629093bf7b0539c3616
devCadet <satyam.karn3629@gmail.com>
2023-07-15 17:39:11 +0530

    Landing Page - initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const timeLine = gsap.timeline({defaults: {ease: 'power2.out'}});
+
+timeLine.to(".slider", {y: "-100%", duration:1.2});
+
+timeLine.to(".intro .hero", {y: "0%", duration:1.2}, "-=1");
+
+timeLine.fromTo(".nav",{opacity:0}, {opacity:1 , duration:1});
